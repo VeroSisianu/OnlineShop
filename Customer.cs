072f3edf2cd6bd2ef32ad08efb50b765ec89f0a3
VeroSisianu <nica220196@gmail.com>
2021-04-19 20:28:51 +0300

    Added User, Customer, Manager, Administrator

---
+++ Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop
+{
+    class Customer : User
+    {
+        DateTime birthDate;
+        string gender;
+        string phoneNumber;
+    }
+}
---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop
+{
+    abstract class User
+    {
+        string name;
+        string username;
+        string password;
+        DateTime registrationDate;
+    }
+}
